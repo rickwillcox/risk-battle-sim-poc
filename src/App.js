4126f38f69cde5f1154a442f49e5f0bb092b2c59
Rick Willcox <rickwillcox90@gmail.com>
2024-03-09 07:51:14 +1000

    feat: poc for risk calc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Slider } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
+import { Chart } from "./chart";
+import { data } from "./data";
 function App() {
+  const [attackers, setAttackers] = useState(1);
+  const [defenders, setDefenders] = useState(1);
+
+  const handleAttackersChange = (event, newValue) => {
+    setAttackers(newValue);
+  };
+
+  const handleDefendersChange = (event, newValue) => {
+    setDefenders(newValue);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Box sx={{ width: "50%", mx: "auto" }}>
+        {" "}
+        {/* Controls the max width and centers the content */}
+        <Typography gutterBottom align="center">
+          Attackers
+        </Typography>
+        <Typography gutterBottom align="center">
+          {attackers}
+        </Typography>
+        <Slider
+          defaultValue={1}
+          aria-label="Attackers"
+          valueLabelDisplay="auto" // Always displays the value label
+          step={1}
+          marks
+          min={1}
+          max={50}
+          onChange={handleAttackersChange}
+        />
+        <Typography gutterBottom align="center" mt={4}>
+          {" "}
+          {/* Adds margin top for spacing */}
+          Defenders
+        </Typography>
+        <Typography gutterBottom align="center">
+          {defenders}
+        </Typography>
+        <Slider
+          defaultValue={1}
+          aria-label="Defenders"
+          valueLabelDisplay="auto" // Always displays the value label
+          step={1}
+          marks
+          min={1}
+          max={40}
+          onChange={handleDefendersChange}
+        />
+      </Box>
+      <Chart attackers={attackers} defenders={defenders} data={data} />
     </div>
   );
 }
---
+++ src/chart.js
@@ -0,0 +1,164 @@
+import React from "react";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import annotationPlugin from "chartjs-plugin-annotation";
+
+// Register the necessary components and the annotation plugin
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  annotationPlugin
+);
+
+const calculatePercentiles = (dataset) => {
+  // Sort the dataset by x value (attackers left)
+  const sortedData = dataset.sort((a, b) => a.x - b.x);
+  const total = sortedData.reduce((acc, { y }) => acc + y, 0);
+  let cumulative = 0;
+
+  const percentiles = {};
+
+  sortedData.forEach(({ x, y }) => {
+    cumulative += y;
+    const percentile = cumulative / total;
+
+    if (!percentiles["25"] && percentile >= 0.25) {
+      percentiles["25"] = x;
+    }
+    if (!percentiles["50"] && percentile >= 0.5) {
+      percentiles["50"] = x;
+    }
+    if (!percentiles["75"] && percentile >= 0.75) {
+      percentiles["75"] = x;
+    }
+    if (!percentiles["90"] && percentile >= 0.9) {
+      percentiles["90"] = x;
+    }
+  });
+
+  return percentiles;
+};
+
+export const Chart = ({ attackers, defenders, data }) => {
+  const key = `${attackers}-${defenders}`;
+  const dataset = data[key].map(([x, y]) => ({ x: `${x}`, y }));
+
+  const percentiles = calculatePercentiles(dataset);
+
+  const chartData = {
+    labels: dataset.map((d) => d.x),
+    datasets: [
+      {
+        label: "Number of Occurrences",
+        data: dataset.map((d) => d.y),
+        backgroundColor: "rgba(54, 162, 235, 0.5)",
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      x: {
+        type: "category",
+        title: {
+          display: true,
+          text: "Attackers Left",
+        },
+      },
+      y: {
+        title: {
+          display: true,
+          text: "Number of Occurrences",
+        },
+      },
+    },
+    plugins: {
+      legend: {
+        display: true,
+      },
+      title: {
+        display: true,
+        text: `Distribution for ${attackers} Attackers vs ${defenders} Defenders on a Capital`,
+      },
+      annotation: {
+        annotations: {
+          // 25: {
+          //   type: "line",
+          //   xMin: percentiles["25"],
+          //   xMax: percentiles["25"],
+          //   borderColor: "rgb(255, 99, 132)",
+          //   borderWidth: 2,
+          //   label: {
+          //     enabled: true,
+          //     content: "25%",
+          //   },
+          // },
+          "25th Percentile": {
+            type: "line",
+            xMin: percentiles["25"],
+            xMax: percentiles["25"],
+            borderColor: "rgb(255, 99, 132)",
+            borderWidth: 2,
+            label: {
+              enabled: true,
+              content: "25%",
+              position: "start",
+              color: "rgb(255, 99, 132)",
+              backgroundColor: "rgba(255,255,255,0.5)",
+              font: {
+                weight: "bold",
+              },
+            },
+          },
+          50: {
+            type: "line",
+            xMin: percentiles["50"],
+            xMax: percentiles["50"],
+            borderColor: "rgb(53, 162, 235)",
+            borderWidth: 2,
+            label: {
+              enabled: true,
+              content: "50%",
+            },
+          },
+          75: {
+            type: "line",
+            xMin: percentiles["75"],
+            xMax: percentiles["75"],
+            borderColor: "rgb(75, 192, 192)",
+            borderWidth: 2,
+            label: {
+              enabled: true,
+              content: "75%",
+            },
+          },
+          90: {
+            type: "line",
+            xMin: percentiles["90"],
+            xMax: percentiles["90"],
+            borderColor: "rgb(255, 205, 86)",
+            borderWidth: 2,
+            label: {
+              enabled: true,
+              content: "90%",
+            },
+          },
+        },
+      },
+    },
+  };
+
+  return <Bar options={options} data={chartData} />;
+};
